38e0cddd16db551543327bcc957cd912af760ef2
Andy Lai <andylai@amazon.com>
2021-11-02 08:17:33 +0000

    Add scheduler to data daemons lambda which will allow it to run periodically

--- lib/audit_service.ts
+++ lib/audit_service.ts
@@ -5,6 +5,8 @@ import * as apigateway from "@aws-cdk/aws-apigateway";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as secretsManager from "@aws-cdk/aws-secretsmanager";
+import * as aws_events from "@aws-cdk/aws-events";
+import * as aws_events_targets from "@aws-cdk/aws-events-targets";
 
 const IncidentBucketRootName = "incidents"
 const CACHE_FILE_NAME = "audit_report"
@@ -13,9 +15,13 @@ export class AuditService extends core.Construct {
   constructor(scope: core.Construct, id: string) {
     super(scope, id);
 
+    // S3 Bucket to store snapshot of audit report
     const bucket = new s3.Bucket(this, IncidentBucketRootName);
+
+    // Store credentials to access the mock service
     const elevateCredentials = new secretsManager.Secret(this, 'ElevateSecretCredentials');
 
+    // Lambda handler that serves the API logic
     const apiGatewayHandler = new lambda.Function(this, 'requestHandler', {
       handler: 'activity.handler',
       runtime: lambda.Runtime.PYTHON_3_8,
@@ -27,6 +33,20 @@ export class AuditService extends core.Construct {
       },
     });
 
+    // API gateway that serves the API
+    const api = new apigateway.RestApi(this, "SecurityAuditAPIGateway", {
+      restApiName: "Elevate Security Audit API",
+      description: "This service provides some audit information on user access telemetry."
+    });
+
+    const getHandler = new apigateway.LambdaIntegration(apiGatewayHandler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", getHandler);
+
+    // Lambda that serves business logic of a daemon that periodically uploads a snapshot of
+    // audit report to S3
     const dataDaemon = new lambda.Function(this, 'dataDaemon', {
       handler: 'data_daemon.handler',
       runtime: lambda.Runtime.PYTHON_3_8,
@@ -39,16 +59,13 @@ export class AuditService extends core.Construct {
       },
     });
 
-    const api = new apigateway.RestApi(this, "SecurityAuditAPIGateway", {
-      restApiName: "Elevate Security Audit API",
-      description: "This service provides some audit information on user access telemetry."
-    });
+    // Scheduler resources that trigger the data daemon's lambda to fire periodically
+    const dataDaemonEventTarget = new aws_events_targets.LambdaFunction(dataDaemon)
 
-    const getHandler = new apigateway.LambdaIntegration(apiGatewayHandler, {
-      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    new aws_events.Rule(this, 'ScheduleRule', {
+      schedule: aws_events.Schedule.cron({ minute: '20'}),
+      targets: [dataDaemonEventTarget],
     });
-
-    api.root.addMethod("GET", getHandler);
 
     // Give permission to lambda to read credentials stored in Secret Manager
     elevateCredentials.grantRead(dataDaemon.role!);
